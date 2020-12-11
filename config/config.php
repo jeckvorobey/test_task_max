c092e887de279d70448b60defd0908bab5c22beb
Sergey Vorobev <jeckvorobey69@gmail.com>
2020-12-11 16:12:54 +0300

    отредактировал некоторые функции

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ const ROOT = __DIR__;
 const ERR_LOG_DIRECTORY = '/files/err/';
 const PHOTO_DIRECTORY = '/../files/photo/';
 const TMP_PHOTO_DIREEKTORY = '/../files/photoTemp/';
-const URL_LOCAL = 'http://localhost:8000/index.php';
+const URL_LOCAL = 'http://localhost:8000/';
 const URL_API = 'http://merlinface.com:12345/api/';
 
 /**
